af0ed59276e26c63a0a88768a1f567f4b5884ed6
Arda Tiryaki <47118347+Overdone123@users.noreply.github.com>
2019-10-15 14:19:29 +0100

    Delete template.php

--- template.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Bounce » Version ONE</title>
-	<link rel="stylesheet" type="text/css" href="assets/main/app.css?<?php echo time(); ?>">
-	<link rel="stylesheet" type="text/css" href="assets/ext/font-awesome/css/all.css?<?php echo time(); ?>">
-	<style type="text/javascript" src="assets/ext/jquery/jquery.js?<?php echo time(); ?>"></style>
-</head> 
-<body>
